e8e4c0b81c65d64bb612a0ae10ca7161f78dedc4
Mike V <mvee333@gmail.com>
2020-09-22 12:32:36 -0400

    Updates

---
+++ precache-manifest.879431f0a55d29518059eb7b80ff3354.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3bb446a09d6931e88ec7b7bb2317a7",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e3cbe5baa7143f3b0b78",
+    "url": "/react-portfolio/static/css/2.1f35d23a.chunk.css"
+  },
+  {
+    "revision": "7da82d4506de9c0c683a",
+    "url": "/react-portfolio/static/css/main.adac044f.chunk.css"
+  },
+  {
+    "revision": "e3cbe5baa7143f3b0b78",
+    "url": "/react-portfolio/static/js/2.99bf1bc8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.99bf1bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da82d4506de9c0c683a",
+    "url": "/react-portfolio/static/js/main.398f5f73.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "b0042442ca3983e3e804e29859b90420",
+    "url": "/react-portfolio/static/media/developerResume.b0042442.pdf"
+  },
+  {
+    "revision": "11f3a256ce5229b4f9cf51c53115168f",
+    "url": "/react-portfolio/static/media/guruPic.11f3a256.png"
+  },
+  {
+    "revision": "802b3df0e749671730181270cae47390",
+    "url": "/react-portfolio/static/media/kickTout.802b3df0.JPG"
+  },
+  {
+    "revision": "4f263ba1f4758400350c1b4fc89c21ad",
+    "url": "/react-portfolio/static/media/planetPic.4f263ba1.png"
+  },
+  {
+    "revision": "6749b137846089ae6df75dc9b7d8ab38",
+    "url": "/react-portfolio/static/media/profPic.6749b137.png"
+  }
+]);
