da1b9501806b5dfce659f45eb2f96138d073d78a
Mike V <mvee333@gmail.com>
2020-09-22 14:45:59 -0400

    Updates

---
+++ precache-manifest.ea5e49ba3d59c2f9a0a41305b18f3c10.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710da0e8bbac52c7666b8e98ca2952f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b6bf9cc43d8fb765f58",
+    "url": "./static/css/2.1f35d23a.chunk.css"
+  },
+  {
+    "revision": "a572a23fffbf30608584",
+    "url": "./static/css/main.adac044f.chunk.css"
+  },
+  {
+    "revision": "7b6bf9cc43d8fb765f58",
+    "url": "./static/js/2.bb38fc05.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.bb38fc05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a572a23fffbf30608584",
+    "url": "./static/js/main.ac7da3ff.chunk.js"
+  },
+  {
+    "revision": "d900f9fd8e1f352a9245",
+    "url": "./static/js/runtime-main.bb27c588.js"
+  },
+  {
+    "revision": "b0042442ca3983e3e804e29859b90420",
+    "url": "./static/media/developerResume.b0042442.pdf"
+  },
+  {
+    "revision": "11f3a256ce5229b4f9cf51c53115168f",
+    "url": "./static/media/guruPic.11f3a256.png"
+  },
+  {
+    "revision": "802b3df0e749671730181270cae47390",
+    "url": "./static/media/kickTout.802b3df0.JPG"
+  },
+  {
+    "revision": "4f263ba1f4758400350c1b4fc89c21ad",
+    "url": "./static/media/planetPic.4f263ba1.png"
+  },
+  {
+    "revision": "6749b137846089ae6df75dc9b7d8ab38",
+    "url": "./static/media/profPic.6749b137.png"
+  }
+]);
